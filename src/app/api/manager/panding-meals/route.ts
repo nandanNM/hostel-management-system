3bd2299afc5006b7d73062b7cc97cab91d02152e
CoderNandan <nandanindia1947@gmail.com>
2025-07-13 04:20:40 +0530

    add guest panding request route

---
+++ src/app/api/manager/panding-meals/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/db";
+import { guestmeal } from "@/db/schemas";
+import getSession from "@/lib/get-session";
+import { getTimeOfDay } from "@/lib/utils";
+import { endOfDay, startOfDay } from "date-fns";
+import { and, between, eq } from "drizzle-orm";
+
+export async function GET() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user.id)
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    if (session.user.role !== "manager")
+      return Response.json(
+        { error: "Unauthorized - You are not a manager" },
+        { status: 401 },
+      );
+    const timeOfDay = getTimeOfDay();
+    const todayStart = startOfDay(new Date()).toISOString();
+    const todayEnd = endOfDay(new Date()).toISOString();
+    const data = await db.query.guestmeal.findFirst({
+      where: and(
+        eq(guestmeal.mealTime, timeOfDay),
+        between(guestmeal.createdAt, todayStart, todayEnd),
+      ),
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
